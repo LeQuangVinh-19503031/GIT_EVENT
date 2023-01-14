dfbeb53775e074ee912b3dedb6d9b19b7146a64b
LeQuangVinh-19503031 <lea26462@gmail.com>
2023-01-14 12:55:02 +0700

    One-path-in-Youtuber
    good

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.git_event {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.git_event.Kteam to javafx.fxml;
-    exports com.example.git_event.Kteam;
-}
